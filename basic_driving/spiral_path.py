b4b6ec5a2aa4138a03dd4369d04571ba622906d6
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2024-07-19 15:32:52 -0400

    Create spiral_path.py

---
+++ basic_driving/spiral_path.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class SpiralDriver:
+    def __init__(self):
+        rospy.init_node('spiral_driver')
+
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.current_linear_velocity = 0.0
+        self.current_angular_velocity = 1
+        self.speed_increment = 0.025
+        self.state = True
+
+    def drive_in_spiral(self):
+        twist = Twist()
+
+        while not rospy.is_shutdown():
+            twist.linear.x = self.current_linear_velocity
+            twist.angular.z = self.current_angular_velocity
+
+            self.pub.publish(twist)
+            
+            if self.current_linear_velocity>=1:
+                self.state=False
+
+            if not self.state and 0<=self.current_linear_velocity<0.025:
+                self.current_linear_velocity=0
+                self.pub.publish(twist)
+                rospy.loginfo("Spiral complete")
+                return
+
+            if self.state:
+                self.current_linear_velocity+=self.speed_increment
+            else:
+                self.current_linear_velocity-=self.speed_increment
+
+
+            #rospy.loginfo(self.current_linear_velocity)
+
+            rospy.sleep(0.25)
+
+if __name__ == '__main__':
+    try:
+        driver = SpiralDriver()
+        driver.drive_in_spiral()
+    except rospy.ROSInterruptException:
+        pass
